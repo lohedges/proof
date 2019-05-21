efd5060764e36affdc1f834c9f0479def9068fe1
Matt Williams <matt@milliams.com>
2019-05-21 12:59:36 +0100

    Speed up value shifting

--- find_filaments_ridges.py
+++ find_filaments_ridges.py
@@ -13,8 +13,11 @@ for f in Path(".").glob("*.mrc"):
 
     start = time.time()
 
-    shifted = (d - d.min()) / ((d.max() - d.min()) / 255)
-    d_cv = shifted.astype(np.uint8)
+    d_min = d.min()
+    d_max = d.max()
+    shifted = d - d_min
+    shifted /= ((d_max - d_min) / 255)
+    d_cv = d.astype(np.uint8)
     d_cv = cv2.equalizeHist(d_cv)
     scale = 8
     d_cv = cv2.resize(d_cv, (int(d.shape[0]/scale), int(d.shape[0]/scale)), interpolation=cv2.INTER_AREA)
