54a046fd3dae5de8b43aac35bff776e281bd5583
Matt Williams <matt@milliams.com>
2019-05-31 18:23:34 +0100

    Use contours to find vectors

--- find_filaments_ridges.py
+++ find_filaments_ridges.py
@@ -34,6 +34,8 @@ for f in Path(".").glob("*.mrc"):
     # TODO Maybe look at fast arguments for this
     distances = cv2.distanceTransform(cv2.bitwise_not(centres), distanceType=cv2.DIST_L2, maskSize=cv2.DIST_MASK_PRECISE)
 
+    contours = cv2.findContours(centres.astype(np.uint8), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
     end = time.time()
     print("Line finding", end - start)
 
@@ -43,10 +45,19 @@ for f in Path(".").glob("*.mrc"):
     overlay = np.where(centres, 3, np.nan)
     plt.cm.Set1.set_bad(color="#00000000")
     cv_image_ax.imshow(overlay, cmap='Set1')
+    cv_image_ax.set_xlim(365, 405)
+    cv_image_ax.set_ylim(300, 260)
 
+    from matplotlib.lines import Line2D
     cv_image_fig = plt.figure(figsize=(10, 9))
     cv_image_ax = cv_image_fig.subplots()
-    cv_image_ax.imshow(distances, cmap="bone")
+    cv_image_ax.imshow(d_cv, cmap="bone")
+    cv_image_ax.set_xlim(365, 405)
+    cv_image_ax.set_ylim(300, 260)
+    for c in contours[0][30:30+1]:
+        c = c[:, 0, :]
+        line = Line2D(c[:, 0], c[:, 1])
+        cv_image_ax.add_line(line)
 
     plt.show()
 
