7318ccb821d21d4b491f688242f6113a07bb0ce1
Matt Williams <matt@milliams.com>
2019-06-06 15:52:42 +0100

    Rescale the picked points correctly

--- star.py
+++ star.py
@@ -13,14 +13,17 @@ for f in Path(".").glob("FoilHole_24681684_Data_24671838_24671839_20181024_1545-
         d = mrc.data
 
     d_cv = image.normalise_8bit(d)
+    d_cv = image.scale_down(d_cv, 8)
 
     star_filename = f"{f.stem}_manualpick.star"
 
     df = pd.read_csv(f"{f.stem}_manualpick.star", delim_whitespace=True, skiprows=10, names=["x", "y", "class_number", "angle_psi", "fom"], usecols=[0, 1])
+    df["x"] = df["x"] / (d.shape[1]/d_cv.shape[1])
+    df["y"] = df["y"] / (d.shape[0]/d_cv.shape[0])
 
     cv_image_fig = plt.figure(figsize=(10, 9))
     cv_image_ax = cv_image_fig.subplots()
     cv_image_ax.imshow(d_cv, cmap="bone")
-    df.plot.scatter("x", "y", ax=cv_image_ax, color="none", facecolors='none', edgecolors='red', s=500)
+    df.plot.scatter("x", "y", ax=cv_image_ax, color="none", facecolors='none', edgecolors='red', s=1000)
 
     plt.show()
